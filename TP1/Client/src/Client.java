061a723d814aee0f50ecaab9a17a3e1cff5e6703
Mathieu <mathieu.chateauvert@polymtl.ca>
2018-10-21 14:38:04 -0400

    Initial

--- TP1/Client/src/Client.java
+++ TP1/Client/src/Client.java
@@ -1,180 +1,88 @@
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.io.PrintWriter;
-//import java.net.Socket;
-//
-//import javax.swing.JTextField;
-//
-//public class Client {
-//	
-//    private BufferedReader in;
-//    private PrintWriter out;
-//    private JTextField dataField = new JTextField(40);
-////    private JFrame frame = new JFrame("Capitalize Client");
-////    private JTextArea messageArea = new JTextArea(8, 60);
-//
-//    /**
-//     * Constructs the client by laying out the GUI and registering a
-//     * listener with the textfield so that pressing Enter in the
-//     * listener sends the textfield contents to the server.
-//     */
-//    public Client() {
-//    	
-//    	
-//        // Add Listeners
-//        dataField.addActionListener(new ActionListener() {
-//
-//            public void actionPerformed(ActionEvent e) {
-//                out.println(dataField.getText());
-//                out.println("allo");
-//                String response = System.console().readLine();
-//                try {
-//                    response = in.readLine();
-//                    if (response == null || response.equals("")) {
-//                          System.exit(0);
-//                      }
-//                } catch (IOException ex) {
-//                       response = "Error: " + ex;
-//                }
-//                System.out.println(response + "\n");
-//                dataField.selectAll();
-//            }
-//        });
-//    }
-//
-//    /**
-//     * Implements the connection logic by prompting the end user for
-//     * the server's IP address, connecting, setting up streams, and
-//     * consuming the welcome messages from the server.  The Capitalizer
-//     * protocol says that the server sends three lines of text to the
-//     * client immediately after establishing a connection.
-//     */
-//    @SuppressWarnings("resource")
-//	public void connectToServer() throws IOException {
-//
-//        // Get the server address from a dialog box.
-//    	System.out.println("Enter IP Address of the Client:");
-//    	String serverAddress = System.console().readLine();
-//    	int port = 5000;
-//
-//        Socket socket;
-//		socket = new Socket(serverAddress, port);
-//		
-//        System.out.format("The capitalization server is running on %s:%d%n", serverAddress, port);
-//        
-//        in = new BufferedReader(
-//                new InputStreamReader(socket.getInputStream()));
-//        out = new PrintWriter(socket.getOutputStream(), true);
-//
-//        // Consume the initial welcoming messages from the server
-//        for (int i = 0; i < 3; i++) {
-//            System.out.println(in.readLine() + "\n");
-//        }
-//    }
-//
-//    /**
-//     * Runs the client application.
-//     */
-//    public static void main(String[] args) throws Exception {
-//        Client client = new Client();
-////        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-////        client.frame.pack();
-////        client.frame.setVisible(true);
-//        client.connectToServer();
-//    }
-//}
-// Source :
-// Ray Toal
-// Department of Electrical Engineering and Computer Science
-// Loyola Marymount University
-// http://cs.lmu.edu/~ray/notes/javanetexamples/
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.regex.Pattern;
+import java.lang.String;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-
-/**
- * A simple Swing-based client for the capitalization server.
- * It has a main frame window with a text field for entering
- * strings and a textarea to see the results of capitalizing
- * them.
- */
 public class Client {
 
     private BufferedReader in;
     private PrintWriter out;
-    private JFrame frame = new JFrame("Capitalize Client");
-    private JTextField dataField = new JTextField(40);
-    private JTextArea messageArea = new JTextArea(8, 60);
+    private String messageArea = new String();
 
-    /**
-     * Constructs the client by laying out the GUI and registering a
-     * listener with the textfield so that pressing Enter in the
-     * listener sends the textfield contents to the server.
-     */
     public Client() {
 
-        // Layout GUI
-        messageArea.setEditable(false);
-        frame.getContentPane().add(dataField, "North");
-        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
-
-        // Add Listeners
-        dataField.addActionListener(new ActionListener() {
-            /**
-             * Responds to pressing the enter key in the textfield
-             * by sending the contents of the text field to the
-             * server and displaying the response from the server
-             * in the text area.  If the response is "." we exit
-             * the whole application, which closes all sockets,
-             * streams and windows.
-             */
-            public void actionPerformed(ActionEvent e) {
-                out.println(dataField.getText());
-                   String response;
-                try {
-                    response = in.readLine();
-                    if (response == null || response.equals("")) {
-                          System.exit(0);
-                      }
-                } catch (IOException ex) {
-                       response = "Error: " + ex;
-                }
-                messageArea.append(response + "\n");
-                dataField.selectAll();
-            }
-        });
     }
 
-    /**
-     * Implements the connection logic by prompting the end user for
-     * the server's IP address, connecting, setting up streams, and
-     * consuming the welcome messages from the server.  The Capitalizer
-     * protocol says that the server sends three lines of text to the
-     * client immediately after establishing a connection.
-     */
+	private final Pattern PATTERN = Pattern.compile(
+			"^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$"); // Pattern pour l'adresse IP
+	
+	public boolean validateIp(final String ip) {
+		return PATTERN.matcher(ip).matches();
+	}
+	
+	public boolean validatePort(final int port) { // Pattern pour le port
+		if (port >= 5000 && port <= 5500){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+    public String initializeIp(){
+        log("Enter IP Address of the Client:");
+  	    String serverAddress = System.console().readLine();
+  	    
+  	    // If IP address entered is wrong.
+        while (!validateIp(serverAddress)){
+            log("Wrong IP Address. Enter another one:");
+        	serverAddress = System.console().readLine();
+        }
+        return serverAddress;
+    }
+	
+    public int initializePort(){
+        log("Enter Port for the Client :");
+        int port = Integer.parseInt(System.console().readLine());
+        
+        // If Port entered is wrong.
+        while (!validatePort(port)){
+            log("Wrong Port. Should be between 5000 and 5500. Enter another one:");
+            port = Integer.parseInt(System.console().readLine());
+        }
+        return port;
+    }
+    
+    private boolean isCommandValid(String command) {
+    	
+    	return (command.equals("ls") || command.equals("exit") ||
+    			firstWordFromCommand(command).equals("cd") ||
+    			firstWordFromCommand(command).equals("mkdir") ||
+    			firstWordFromCommand(command).equals("upload") ||
+    			firstWordFromCommand(command).equals("download"));
+    }
+    
+    private String firstWordFromCommand(String command){
+    	String firstWord = command;
+        if (firstWord.contains(" ")){
+        	firstWord= firstWord.substring(0, firstWord.indexOf(" "));
+        }
+        return firstWord;
+    }
+	
+    private void log(String message) {
+        System.out.println(message);
+    }
+    
     @SuppressWarnings("resource")
 	public void connectToServer() throws IOException {
-
-        // Get the server address from a dialog box.
-        String serverAddress = JOptionPane.showInputDialog(frame,"Enter IP Address of the Server:","Welcome to the Capitalization Program",JOptionPane.QUESTION_MESSAGE);
-        int port = 5000;
-
-        Socket socket;
-		socket = new Socket(serverAddress, port);
+        
+    	String serverAddress = initializeIp();
+        int port = initializePort();
+        Socket socket = new Socket(serverAddress, port);
 		
         System.out.format("The capitalization server is running on %s:%d%n", serverAddress, port);
         
@@ -184,18 +92,34 @@ public class Client {
 
         // Consume the initial welcoming messages from the server
         for (int i = 0; i < 3; i++) {
-            messageArea.append(in.readLine() + "\n");
+            messageArea += in.readLine() + "\n";
         }
+        log(messageArea);
+        
+        String command = "";
+        while (!isCommandValid(command)){
+        	log("Enter Command");
+        	command = System.console().readLine();
+        }
+        out.println(command);
+        
+        
+        
+        String response;
+        try {
+            response = in.readLine();
+            if (response == null || response.equals("")) {
+                  System.exit(0);
+              }
+        } catch (IOException ex) {
+               response = "Error: " + ex;
+        }
+        log(response);
+        
     }
 
-    /**
-     * Runs the client application.
-     */
     public static void main(String[] args) throws Exception {
         Client client = new Client();
-        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        client.frame.pack();
-        client.frame.setVisible(true);
         client.connectToServer();
     }
 }
